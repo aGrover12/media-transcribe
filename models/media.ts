4ff333c120da6cda9696724e34ba86fe8e8ef8d0
Auston Grover <austongrover@yahoo.com>
2023-08-06 10:44:34 -0500

    init

---
+++ models/media.ts
@@ -0,0 +1,5 @@
+export class Media {
+    id: Number
+    title: String
+    directory: String
+}
---
+++ services/MediaActions.ts
@@ -0,0 +1,11 @@
+
+import { Media }  from "../models/media"
+export class MediaActions {
+    public uploadMedia(media : Media) {
+        // We'll upload information to database from here
+    }
+
+    public retrieveMedia(id: Number) {
+        // We'll pass the id to the database from here
+    }
+}
